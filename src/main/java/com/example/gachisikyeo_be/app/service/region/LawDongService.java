6ee158f6407e848cb1ce5ee0a08ac92a4366cb7d
Lee Seungchang <jjekrrq01@naver.com>
2025-12-03 15:59:56 +0900

    perf : add Transactional(readOnly = true) to LawDongService

--- src/main/java/com/example/gachisikyeo_be/app/service/region/LawDongService.java
+++ src/main/java/com/example/gachisikyeo_be/app/service/region/LawDongService.java
@@ -3,6 +3,7 @@ package com.example.gachisikyeo_be.app.service.region;
 import com.example.gachisikyeo_be.app.repository.region.LawDongRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -12,14 +13,17 @@ public class LawDongService {
 
     private final LawDongRepository lawDongRepository;
 
+    @Transactional(readOnly = true)
     public List<String> getSidoList() {
         return lawDongRepository.findAllSido();
     }
 
+    @Transactional(readOnly = true)
     public List<String> getSigunguList(String sido) {
         return lawDongRepository.findSigunguBySido(sido);
     }
 
+    @Transactional(readOnly = true)
     public List<String> getDongList(String sido, String sigungu) {
         return lawDongRepository.findDongBySidoAndSigungu(sido, sigungu);
     }
