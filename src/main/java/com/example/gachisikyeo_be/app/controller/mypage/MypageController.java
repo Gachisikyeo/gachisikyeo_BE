b9e2cbd3daebca32811a4f1a92a3e0c959b3ec70
Choi, Yoora <liveoh387@gmail.com>
2025-12-21 14:22:20 +0900

    fix: 마이페이지 조회 시 principal 타입 불일치 오류  수정

--- src/main/java/com/example/gachisikyeo_be/app/controller/mypage/MypageController.java
+++ src/main/java/com/example/gachisikyeo_be/app/controller/mypage/MypageController.java
@@ -2,6 +2,8 @@ package com.example.gachisikyeo_be.app.controller.mypage;
 
 import com.example.gachisikyeo_be.app.dto.mypage.MypageResponseDto;
 import com.example.gachisikyeo_be.app.service.mypage.MypageService;
+import com.example.gachisikyeo_be.global.code.ErrorCode;
+import com.example.gachisikyeo_be.global.exception.AuthException;
 import com.example.gachisikyeo_be.global.users.domain.auth.CustomUserDetails;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
@@ -21,11 +23,14 @@ public class MypageController {
     @Operation(summary = "마이페이지 조회",
             description = "기본 마이페이지 창을 조회합니다.",
             security = @SecurityRequirement(name = "bearerAuth"))
-    @GetMapping
+    @GetMapping("/main")
     public MypageResponseDto getMypage(
-            @AuthenticationPrincipal CustomUserDetails userDetails
+            @AuthenticationPrincipal String userId
     ) {
-        Long userId = userDetails.getUserId();
-        return mypageService.getMypage(userId);
+        if (userId == null) {
+            throw new AuthException(ErrorCode.AUTH_REQUIRED); //인증 필요한 유저
+        }
+
+        return mypageService.getMypage(Long.parseLong(userId));
     }
 }
--- src/main/java/com/example/gachisikyeo_be/app/dto/mypage/MypageResponseDto.java
+++ src/main/java/com/example/gachisikyeo_be/app/dto/mypage/MypageResponseDto.java
@@ -2,13 +2,15 @@ package com.example.gachisikyeo_be.app.dto.mypage;
 
 import com.example.gachisikyeo_be.app.domain.region.LawDong;
 import com.example.gachisikyeo_be.global.users.domain.auth.UserType;
+import lombok.Getter;
 
+@Getter
 public class MypageResponseDto {
 
-    private String nickname;
-    private String email;
-    private String lawDong;
-    private UserType userType;
+    private final String nickname;
+    private final String email;
+    private final String lawDong;
+    private final UserType userType;
 
     public MypageResponseDto(String nickname, String email, String lawDong, UserType userType) {
         this.nickname = nickname;
