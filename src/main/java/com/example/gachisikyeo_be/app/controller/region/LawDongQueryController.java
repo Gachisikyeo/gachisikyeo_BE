03fcb905b7ba26f371b000d3d02921a0f27a07d8
Lee Seungchang <jjekrrq01@naver.com>
2025-12-22 10:40:44 +0900

    refactor : swagger의 error response 설정

--- src/main/java/com/example/gachisikyeo_be/app/controller/region/LawDongQueryController.java
+++ src/main/java/com/example/gachisikyeo_be/app/controller/region/LawDongQueryController.java
@@ -3,9 +3,13 @@ package com.example.gachisikyeo_be.app.controller.region;
 import com.example.gachisikyeo_be.app.dto.LawDongDto;
 import com.example.gachisikyeo_be.app.service.region.LawDongService;
 import com.example.gachisikyeo_be.global.code.SuccessCode;
+import com.example.gachisikyeo_be.global.responseTemplate.ApiErrorResponseForSwagger;
 import com.example.gachisikyeo_be.global.responseTemplate.ApiResponseTemplate;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -64,8 +68,14 @@ public class LawDongQueryController {
     }
 
     @Operation(summary = "시도, 시군구, 동으로 지역 얻기", description = "선택한 시/도와 시/군/구, 동으로 설정 지역을 얻기")
-    @ApiResponse(responseCode = "200", description = "LawDong 얻기 성공")
-    @ApiResponse(responseCode = "404", description = "찾는 지역 조회 실패")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "LawDong 얻기 성공"),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "찾는 지역 조회 실패",
+                    content = @Content(schema = @Schema(implementation = ApiErrorResponseForSwagger.class))
+            )
+    })
     @GetMapping("/resolve")
     public ResponseEntity<ApiResponseTemplate<LawDongDto>> resolveRegion(
             @RequestParam String sido,
