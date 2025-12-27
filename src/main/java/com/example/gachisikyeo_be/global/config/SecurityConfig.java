80dcd7a584ec0b2e4952bc8b2e30b9b0859b08cb
Lee Seungchang <jjekrrq01@naver.com>
2025-12-27 13:23:40 +0900

    fix : yml 수정 및 쿠키 cors 설정

--- src/main/java/com/example/gachisikyeo_be/global/config/SecurityConfig.java
+++ src/main/java/com/example/gachisikyeo_be/global/config/SecurityConfig.java
@@ -49,7 +49,7 @@ public class SecurityConfig {
 
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type", "X-Requested-With"));
-        config.setAllowCredentials(false);
+        config.setAllowCredentials(true);
         config.setMaxAge(3600L);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
